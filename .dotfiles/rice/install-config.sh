b8e3f179058120360ce5ea2ed791f527f8a598a4
al1-ce <al1-ce@null.net>
2025-02-11 16:48:52 +0300

    update

--- .dotfiles/rice/install-config.sh
+++ .dotfiles/rice/install-config.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 copyEtc() {
     echo "Copying /etc/$1"
-    cp -rT ~/.dotfiles/rice/etc/$1 /etc/$1
+    sudo cp -rT $HOME/.dotfiles/rice/etc/$1 /etc/$1
 }
 
 copyEtcFile() {
     echo "Copying /etc/$1"
-    cp ~/.dotfiles/rice/etc/$1 /etc/$1
+    sudo cp $HOME/.dotfiles/rice/etc/$1 /etc/$1
 }
 
 copyEtc greetd
@@ -16,8 +17,8 @@ copyEtcFile pacman.conf
 
 echo "Copying X config to $HOME"
 
-cp ~/.dotfiles/rice/xconfig/.xprofile ~/.xprofile
-cp ~/.dotfiles/rice/xconfig/.xexec ~/.xexec
+cp $HOME/.dotfiles/rice/xconfig/.xprofile $HOME/.xprofile
+cp $HOME/.dotfiles/rice/xconfig/.xexec $HOME/.xexec
 
 echo "Successfully finished"
 
